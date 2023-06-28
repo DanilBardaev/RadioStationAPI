fc56952ab5b5d35242864d7c9a9669d0c47f8b3e
Danil Bardaev <danilgti135@gmail.com>
2023-06-28 17:23:15 +0300

    qwqe

--- js/code.js
+++ js/code.js
@@ -1,81 +1,81 @@
-const input_country = document.querySelector("#country");
-const input_genre = document.querySelector("#genre");
-const button = document.querySelector(".button");
-const radio_list = document.querySelector(".radio_list");
+const input_country = document.querySelector('#country');
+const selection_Genre = document.querySelector('#genre');
+const searchButton = document.querySelector('#search-button');
+const list_station = document.querySelector('#list_station');
 
-const country_list = () => {
+async function country_and_genre(strana) {
+  try {
+    const response = await fetch(`https://de1.api.radio-browser.info/json/stations/bycountry/${strana}`);
+    if (!response.ok) {
+      throw new Error('Сетевая ошибка');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Ошибка:', error);
+    return [];
+  }
+}
+
+const genres = ['classical', 'blues', 'hiphop', 'pop', 'rock', 'jazz', 'hip hop', 'trap', 'pasillo', 'R&B', 'trance', 'house', 'metal', 'pank'];
+genres.forEach(genre => {
+  const option = document.createElement('option');
+  option.value = genre;
+  option.textContent = genre;
+  selection_Genre.appendChild(option);
+});
+
+
+let correspondingStations = [];
+
+searchButton.addEventListener('click', async () => {
+  const country = input_country.value.trim();
+  const genre = selection_Genre.value.trim();
+  const stations = await country_and_genre(country);
+
+  correspondingStations = stations.filter(station => station.tags.includes(genre));
+  
+  if (correspondingStations.length === 0) {
+    list_station.textContent = `Станций с жанром "${genre}" не найдено`;
+    return;
+  }
+  
+  list_station.innerHTML = '';
+  
+  correspondingStations.forEach(station => {
+    const station_linkes = document.createElement('a');
+    station_linkes.href = station.homepage;
+    if (station.homepage === '') {
+      station_linkes.href = station.url 
+    }
+      station_linkes.target = '_blank';
+    station_linkes.textContent = station.name;
+    station_linkes.addEventListener('click', () => {
+      window.open(`https://en.wikipedia.org/wiki/${station.name}`, "popup", "width=800,height=450");
+    });
+    const Items_list = document.createElement('li');
+    Items_list.appendChild(station_linkes);
+    list_station.appendChild(Items_list);
+  });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
   fetch("https://de1.api.radio-browser.info/json/countries/")
     .then((response) => {
-      console.log(response.status);
       if (!response.ok)
         throw new Error(`Ошибка: ${response.status}`);
       return response.json();
     })
     .then((countries) => {
-      const list_country = document.querySelector(".list_country");
+      const input_country = document.querySelector(".list_country");
       countries.forEach((country) => {
         let option = `<option value="${country.iso_3166_1}">${country.name}</option>`;
-        list_country.insertAdjacentHTML("beforeend", option);
+        input_country.insertAdjacentHTML("beforeend", option);
       });
     })
     .catch((error) => {
       console.error(error);
     });
-};
-
-const genre_list = () => {
-  fetch("https://de1.api.radio-browser.info/json/stations/bytag/")
-    .then((response) => {
-      console.log(response.status);
-      if (!response.ok)
-        throw new Error(`Ошибка: ${response.status}`);
-      return response.json();
-    })
-    .then((genres) => {
-      const list_genre = document.querySelector(".list_genre");
-      genres.forEach((genre) => {
-        let option = `<option value="${genre.tags}"</option>`;
-        list_genre.insertAdjacentHTML("beforeend", option);
-      });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
-
-
-const getRadioStations = () => {
-  const country = input_country.value;
-  const genre = input_genre.value;
-
-  fetch(`http://de1.api.radio-browser.info/json/stations/bytag/${genre}`)
-    .then((response) => {
-      console.log(response.status);
-      if (!response.ok)
-        throw new Error(`Ошибка: ${response.status}`);
-      return response.json();
-    })
-    .then((stations) => {
-      radio_list.innerHTML = "";
-      stations.forEach((station) => {
-        let listItem = document.createElement("li");
-        listItem.textContent = station.name;
-        listItem.addEventListener("click", () => {
-          window.open(`https://en.wikipedia.org/wiki/${station.name}`, "popup", "width=800,height=450");
-          let li = `<li class="col-sm col-md"><a href="${station.url}" target="_blank" class="col-sm col-md">Воспроизвести  - ${station.name}</a></li>`;
-          radio_list.insertAdjacentHTML("beforeend", li);
-        });
-        radio_list.appendChild(listItem);
-      });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-  country_list();
-  genre_list();
-
-  button.addEventListener("click", getRadioStations);
 });
